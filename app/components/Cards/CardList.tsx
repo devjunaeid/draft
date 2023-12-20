1293789981e7b4b4f00f07c261b35dadc21041f9
devjunaeid <junaeid.md48@gmail.com>
2023-12-20 12:51:51 +0600

    Blog Card List based on API request

---
+++ app/components/Cards/CardList.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Post from "../Posts/Post";
+import featured from "@/public/featured/fatured.jpg";
+import { JsonArray, JsonObject } from "next-auth/adapters";
+import Pagination from "../Pagination/Pagination";
+import { stringify } from "querystring";
+import Card from "./Card";
+
+const ft = {
+  title: '"Cloud" The next generation of Computing',
+  img: featured,
+  dsc: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+  url: "/",
+  alt: "Testing",
+};
+
+const getdata = async (pageNumber: number, cat: string) => {
+  const res = await fetch(
+    `http://localhost:3000/api/posts?page=${pageNumber}&cat=${cat || ""}`,
+    {
+      cache: "no-cache",
+    }
+  );
+  if (!res.ok) {
+    throw new Error();
+  }
+  return res.json();
+};
+
+async function CardList({ pageNumber, cat }: { pageNumber: string, cat: string }) {
+  const page = parseInt(pageNumber);
+  console.log(page, cat)
+  const { post, count } = await getdata(page, cat);
+
+  console.log(post);
+
+  const POST_PER_PAGE = 2;
+  const hasPrev = POST_PER_PAGE * (page - 1) > 0;
+  const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
+
+  
+  return (
+    <div className="grid grid-cols-1 grid-flow-row gap-4 my-8 w-full">
+      <div>
+        {post?.map((item: any) => (
+          <Card
+            title={item.title}
+            cover={item.img}
+            desc={item.sdesc}
+            author={item.user.name}
+            date={item.createAt}
+            profilePic={item.user.image}
+            tag={item.catSlag}
+            url={item.slug}
+            key={item._id}
+          />
+        ))}
+      </div>
+      <div className="w-full md:col-span-2">
+        <Pagination page={pageNumber} prev={hasPrev} next={hasNext} />
+      </div>
+    </div>
+  );
+}
+
+export default CardList;
