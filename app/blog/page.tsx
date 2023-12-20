2e1d928d658627ff35b1a2a4dec14daf28c1ada2
devjunaeid <junaeid.md48@gmail.com>
2023-12-20 13:02:55 +0600

    Added: CardList, Removed: Dummy Post Cards. Feched CardList based on "searchParams" and "page" value.

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,18 +5,25 @@ import Post from '../components/Posts/Post'
 import profilepic from '@/public/post/author.jpg';
 import cover from '@/public/post/cover.jpg';
 import Card from '../components/Cards/Card';
+import CardList from '../components/Cards/CardList';
 
 const des = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex qui voluptates quae ratione vel voluptas quod repellat explicabo eius consequatur perspiciatis eum aspernatur adipisci pariatur, temporibus vitae iste in expedita." 
 
-function page() {
+function page({
+  searchParams,
+}: {
+  searchParams: {
+    query: string;
+    page: string;
+  };
+}) {
+
+  const currentPage = searchParams.page || "1";
+  const { cat }= searchParams;
   return (
     <div className='flex w-full gap-16'>
       <div className='w-full md:w-2/3 mb-8 gap-4'>
-        <Post title='Hello 123' alt='demo' date='11-11-23' dsc='Testing all at once' img={img} url='/' tag='gg'/>
-        <Post title='Hello 123' alt='demo' date='11-11-23' dsc='Testing all at once' img={img} url='/' tag='gg'/>
-        <Post title='Hello 123' alt='demo' date='11-11-23' dsc='Testing all at once' img={img} url='/' tag='gg'/>
-        <Post title='Hello 123' alt='demo' date='11-11-23' dsc='Testing all at once' img={img} url='/' tag='gg'/>
-        <Card author="Junaeid" cover={cover} date='22-10-19' desc={des} profilePic={profilepic} tag='tech' title='Winter- The harsh reality in middle to lower class family.' url='/blogs'/>
+        <CardList pageNumber={currentPage} cat={cat}/>
       </div>
       <div className='hidden justify-center items-start md:flex md:w-1/3'>
         <Cardsm author='Junaeid' img={img} title='Hello 123' url='/' tag='tech'/>
