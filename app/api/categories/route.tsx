ef159b7dc264dd5a5c327f41b2f7882abf4ea5e9
devjunaeid <juaneid.md48@gmail.com>
2023-12-21 14:17:15 +0600

    Fixed: NextResponse status typo.

--- app/api/categories/route.tsx
+++ app/api/categories/route.tsx
@@ -5,11 +5,11 @@ import { NextResponse } from "next/server";
 export const GET = async () => {
     try {
         const cat = await prisma?.category.findMany();
-        return new NextResponse(JSON.stringify(cat, {status: 200}))
+        return new NextResponse(JSON.stringify(cat), {status: 200})
     } catch (error) {
         console.log(error);
         return new NextResponse(
-            JSON.stringify({message:"Something Went Wrong!!!"}, {status: 500})
+            JSON.stringify({message:"Something Went Wrong!!!"}), {status: 500}
         );
     }
 }
